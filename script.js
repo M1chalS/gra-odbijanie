d2ac3622929cabe0c17eed655c63d57f69d191fe
Michał Szajner <michalszajner@wp.pl>
2023-09-13 18:50:21 +0200

    Bullet enemy, game difficulty based on number of points

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let dirArrow = "right";
 let interval;
+let intervalBullet;
 let points = 0;
 let lives = 3;
+let bullet = false;
 
 function checkCollision() {
     let posX = $("#arrow").position().left;
@@ -14,22 +16,53 @@ function checkCollision() {
         $("#arrow").css("top", Math.floor(Math.random() * 500));
         points++;
         $("#points").html(points);
+        calculateDifficulty();
     }
 }
 
-async function checkLives() {
+function checkCollisionBullet() {
+    let posX = $("#bullet").position().left;
+    let posY = $("#bullet").position().top;
+    let shieldX = $("#shield").position().left;
+    let shieldY = $("#shield").position().top;
+
+    if (posX >= shieldX && posX <= shieldX + 50 && posY >= shieldY && posY <= shieldY + 100) {
+        $("#bullet").css("left", 0);
+        $("#bullet").css("top", Math.floor(Math.random() * 500));
+        points++;
+        $("#points").html(points);
+        calculateDifficulty();
+        bullet = false;
+        $("#bullet").css("display", "none");
+        clearInterval(intervalBullet);
+    }
+}
+
+function checkLives() {
     if (lives === 0) {
-        await alert("Koniec gry, punkty:" + points);
+        gameState.off();
+        alert("Koniec gry, punkty: " + points);
         location.reload();
     }
 }
 
 function checkCollisionWithGround() {
     let posX = $("#arrow").position().left;
+    let posXBullet = $("#bullet").position().left;
 
     if (posX >= 975) {
         lives--;
         $("#lives").html(lives);
+    }
+
+    if(posXBullet >= 960) {
+        $("#bullet").css("left", 0);
+        $("#bullet").css("top", Math.floor(Math.random() * 500));
+        lives--;
+        $("#lives").html(lives);
+        bullet = false;
+        $("#bullet").css("display", "none");
+        clearInterval(intervalBullet);
     }
 }
 
@@ -47,11 +80,43 @@ function calculateArrow() {
     }
 
     if (dirArrow === "right") {
-        $("#arrow").css("left", posX + 15);
+        $("#arrow").css("left", posX + gameParams.arrowSpeed);
     }
 
     if (dirArrow === "left") {
-        $("#arrow").css("left", posX - 15);
+        $("#arrow").css("left", posX - gameParams.arrowSpeed);
+    }
+}
+
+function calculateBullet() {
+    let posX = $("#bullet").position().left;
+    $("#bullet").css("left", posX + gameParams.bulletSpeed);
+}
+
+function calculateDifficulty() {
+    switch (points) {
+        case 3: gameParams.arrowSpeed = 23; break;
+        case 6: gameParams.arrowSpeed = 25; break;
+        case 8: gameParams.arrowSpeed = 28; gameParams.bulletSpeed = 50; break;
+        case 10: gameParams.arrowSpeed = 30; break;
+        case 15: gameParams.arrowSpeed = 33; gameParams.bulletSpeed = 55; break;
+        case 19: gameParams.arrowSpeed = 36; break;
+    } 
+}
+
+function spawnBullet() {
+    console.log(bullet)
+    if((points === 2 || points === 5 || points === 8 || points === 13 || points === 17) && bullet === false) {
+        bullet = true;
+        intervalBullet = setInterval(function() {
+            calculateBullet();
+        }, 100);
+    }
+
+    if(bullet) {
+        $("#bullet").css("display", "block");
+    } else {
+        $("#bullet").css("display", "none");
     }
 }
 
@@ -61,16 +126,23 @@ const gameState = {
         interval = setInterval(function () {
             calculateArrow();
             checkCollision();
+            checkCollisionBullet();
             checkLives();
             checkCollisionWithGround();
+            spawnBullet();
         }, 100)
     },
     off: () => clearInterval(interval)
 };
 
+const gameParams = {
+    arrowSpeed: 20,
+    bulletSpeed: 45,
+    shieldSpeed: 30
+}
+
 $(function () {
     gameState.on();
-    calculateShield();
 
     $("#start").click(function () {
         if (gameState.status) {
@@ -88,18 +160,22 @@ $(function () {
 });
 
 $(document).on("keydown", function (event) {
+    if(!gameState.status) {
+        return;
+    }
+    
     let posY = $("#shield").position().top;
     const key = (event.keyCode ? event.keyCode : event.which);
 
     if(posY > 0) {
-        if (key == '87') {
-            $("#shield").css("top", posY - 30);
+        if (key == '87' || key == '38') {
+            $("#shield").css("top", posY - gameParams.shieldSpeed);
         }
     }
 
     if(posY < 510) {
-         if (key == '83') {
-            $("#shield").css("top", posY + 30);
+         if (key == '83' || key == '40') {
+            $("#shield").css("top", posY + gameParams.shieldSpeed);
         }
     }
 
